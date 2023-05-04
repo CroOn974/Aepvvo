01891c6972fbbd9dc4b50f52cd9d9076ce78a6f9
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2023-05-04 13:17:35 +0200

    Update serializer.py

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -3,7 +3,7 @@ from api.models import Category, Cylinders, Door, Drivewheels , Fuel, Gearbox, M
 
 class ManufacturerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
+        model = Manufacturer
         fields = '__all__'
 
 class ModelSerializer(serializers.ModelSerializer):
@@ -13,7 +13,7 @@ class ModelSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Manufacturer
+        model = Category
         fields = '__all__'
 
 class CylindersSerializer(serializers.ModelSerializer):
