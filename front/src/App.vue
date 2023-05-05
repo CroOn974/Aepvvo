dd76e35401e51182f488b222dc5abc42ed0c5185
Saia Ralandison <ralandison.saia@gmail.com>
2023-05-05 15:40:36 +0400

    Ajout des données en base de données/ ajustement des boutons

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,164 @@
 <template>
-  <router-view/>
+<router-view/>
+<v-container class = "d-flex flex-wrap">
+
+    <v-col align-self="start" lg="5" class="colonnes">
+
+      <v-row class="ligne1" align-content="center" justify="center">
+        
+          <v-text-field
+            align-self="center"
+            bg-color="blue"
+            v-model="firstName"
+            :rules="rules"
+            label="Marque"
+          >
+          </v-text-field>
+        
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Modèle"
+          >
+          </v-text-field>
+
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Taille"
+          >
+          </v-text-field>
+
+          <v-text-field
+            align-self="center"
+            bg-color="blue"
+            v-model="firstName"
+            :rules="rules"
+            label="First name"
+          >
+          </v-text-field>
+
+      </v-row>
+
+
+    <v-row class="ligne2" cols="start">
+
+          <v-text-field
+            align-self="center"
+            bg-color="blue"
+            v-model="firstName"
+            :rules="rules"
+            label="First name"
+          >
+          </v-text-field>
+
+        
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Valeur"
+          >
+          </v-text-field>
+
+          <v-text-field
+            bg-color="yellow"
+            v-model="firstName"
+            :rules="rules"
+            label="Taille"
+          >
+          </v-text-field>
+
+          <v-text-field
+            align-self="center"
+            bg-color="red"
+            v-model="firstName"
+            :rules="rules"
+            label="First name"
+          >
+          </v-text-field>
+
+      </v-row>
+
+      <v-row class="ligne3" cols="start">
+
+          <v-text-field
+            align-self="center"
+            bg-color="blue"
+            v-model="firstName"
+            :rules="rules"
+            label="First name"
+          >
+          </v-text-field>
+
+
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="Valeur"
+            >
+          </v-text-field>
+
+          <v-text-field
+            bg-color="yellow"
+            v-model="firstName"
+            :rules="rules"
+            label="Taille"
+          >
+          </v-text-field>
+
+          <v-text-field
+            align-self="center"
+            bg-color="red"
+            v-model="firstName"
+            :rules="rules"
+            label="First name"
+          >
+          </v-text-field>
+
+          </v-row>
+
+      <v-row class="ligneB" justify="center">
+        <v-col class="colBout"  >
+          <v-btn class="bouttonF" size="large">Calculer le prix</v-btn>
+        </v-col>
+      </v-row>
+
+    </v-col>
+
+</v-container>
 </template>
+
+
+<!-- ///////////////////////////// LE SCRIPT ICI////////////////////////////////////// -->
+<script>
+
+</script>
+
+
+
+<!--////////////////////////////// Ici le CSS //////////////////////////////////////////-->
+<style>
+.colonnes{
+
+}
+.bouttonF { 
+  background-color: black;
+margin: auto; 
+}
+
+.colBout{
+  display: flex;
+}
+
+.ligne1{
+  display: flex;
+}
+
+.ligne2 {
+  display: flex;
+}
+
+.ligne3{
+  display: flex;
+}
+</style>
